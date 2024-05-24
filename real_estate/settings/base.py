db9e95493c857070df2ec81180f5b3a05e40202c
Your Name <you@example.com>
2024-05-24 18:29:34 +0300

    new files

--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -86,12 +86,7 @@ WSGI_APPLICATION = "real_estate.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+
 
 
 # Password validation
