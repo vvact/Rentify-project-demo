407dd90cf4cc0225e793a5c4fbe1fb3839db374f
Your Name <you@example.com>
2024-05-25 01:45:02 +0300

    new changes

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,7 +1,7 @@
 from django.utils import timezone
 from django.db import models
 import uuid
-from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, Group, Permission
 from django.utils.translation import gettext_lazy as _
 
 from .managers import CustomUserManager
@@ -16,6 +16,9 @@ class User(AbstractBaseUser, PermissionsMixin):
     is_staff = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
     date_joined = models.DateTimeField(default=timezone.now)
+
+    groups = models.ManyToManyField(Group, related_name='custom_user_set', blank=True)
+    user_permissions = models.ManyToManyField(Permission, related_name='custom_user_set', blank=True)
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username', 'first_name', 'last_name']
