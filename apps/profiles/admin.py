c7baa15a4627e46203bd460991ecff668eefcc1c
Your Name <you@example.com>
2024-05-25 02:35:18 +0300

    new changes

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
-# Register your models here.
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id','pkid','user','gender','phone_number','country','city']
+    list_display_links = ['id','pkid','user','gender','phone_number','country','city']
+    list_filter = ['gender','phone_number','country','city']
+
+
+admin.site.register(Profile, ProfileAdmin)
