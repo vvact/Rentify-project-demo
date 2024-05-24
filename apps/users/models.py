21a826b4e6909c4d08d143238b05002b5b1961f7
Your Name <you@example.com>
2024-05-25 00:20:06 +0300

    furu

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import uuid
+from django.contrib.auth.models import AbstractBaseUser,PermissionsMixin
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class User(AbstractBaseUser,PermissionsMixin):
+     pkid = models.BigAutoField(primary_key=True,editable=False)
+     id = models.UUIDField(default=uuid.uuid4, editable=False,unique=True)
+     username = models.CharField(verbose_name=_('Username'), max_length=255, unique=True)
+     first_name = models.CharField(validators=_('First Name'), max_length=50)
+     last_name = models.CharField(verbose_name=_('Last Name'), max_length=50)
