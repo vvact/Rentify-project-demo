c444f14e633f9821dd5e0aafa831eeebbe80f3b7
Your Name <you@example.com>
2024-05-28 02:00:44 +0300

    login update

---
+++ apps/profiles/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from .views import (AgentListAPIView,GetProfileAPIView,TopAgentsListAPIView,UpdateProfileAPIView)
+
+urlpatterns = [
+    path("me/", GetProfileAPIView.as_view(), name= "get_profile"),
+    path("agents/all/", AgentListAPIView.as_view(), name= "agent_list"),
+    path("me/update/", UpdateProfileAPIView.as_view(), name= "update_profile"),
+    
+    path("agents/top/", TopAgentsListAPIView.as_view(), name= "top_agents_list"),
+    
+    
+]
--- apps/profiles/views.py
+++ apps/profiles/views.py
@@ -7,3 +7,54 @@ from .exceptions import ProfileNotFound,NotYourProfile
 from .models import Profile
 from .renderers import ProfileJSONREnderer
 from  .serializers import ProfileSerializer, UpdateProfileSerializer
+
+
+class AgentListAPIView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.filter(is_agent = True)
+    serializer_class = ProfileSerializer
+
+
+class TopAgentsListAPIView(generics.ListAPIView):
+    permission_classes = [permissions.IsAuthenticated]
+    queryset = Profile.objects.filter(is_agent = True,top_agent = True)
+    serializer_class = ProfileSerializer
+
+class GetProfileAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [ProfileJSONREnderer]
+
+    def ge(self, request):
+        user = self.request.user
+        user_profile = Profile.objects.get(user = user)
+        serializer = ProfileSerializer(user_profile,context={"request":request})
+        return Response(serializer.data,status=status.HTTP_200_OK)
+    
+class UpdateProfileAPIView(APIView):
+    permission_classes = [permissions.IsAuthenticated]
+    renderer_classes = [ProfileJSONREnderer]
+
+    selializer_class = UpdateProfileSerializer
+
+
+    def patch(self,request,username):
+        try:
+            Profile.objects.get(user__username = username)
+        
+        except Profile.DoesNotExist:
+            raise ProfileNotFound()
+        
+
+        user_name = request.user.username
+        if user_name!= username:
+            raise NotYourProfile()
+        
+        data = request.data
+        serilizer = UpdateProfileSerializer(instance=request.user.profile,data= data, partial = True)
+
+        if serilizer.is_valid():
+            serilizer.save()
+            return Response(serilizer.data,status=status.HTTP_200_OK)
+        else:
+            return Response(serilizer.errors,status=status.HTTP_400_BAD_REQUEST)
+
