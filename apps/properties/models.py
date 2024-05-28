2b68e615cff8d542a267ef0cf96d7d8aa146314f
Your Name <you@example.com>
2024-05-29 01:30:21 +0300

    new files

--- apps/properties/models.py
+++ apps/properties/models.py
@@ -3,11 +3,11 @@ import string
 
 
 from autoslug import AutoSlugField
-from django.contrib.auth import MinValueValidator
+from django.core.validators import MinValueValidator
 from django.db import models
+from django_countries.fields import CountryField
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-from django_countries import CountryField
 from apps.common.models import TimeStampedUUIDModel
 
 
@@ -16,4 +16,188 @@ User = get_user_model()
 
 class PropertyPublishedManager(models.Manager):
     def get_queryset(self):
-        return super(PropertyPublishedManager, self).get_queryset().filter(published=True)
+        return(
+            super(PropertyPublishedManager, self).get_queryset().filter(published=True)
+        )
+    
+class Property(TimeStampedUUIDModel):
+    class AdvertType(models.TextChoices):
+        FOR_SALE = 'SALE', _('Sale')
+        FOR_RENT = 'RENT', _('Rent')
+        AUTION = 'Auction',('Auction')
+
+    class PropertyType(models.TextChoices):
+        SINGLE_ROOM = 'Single_Room', _('Single_Room')
+        DOUBLE_ROOM = 'Double_Room', _('Double_Room')
+        BEDSITTER = 'Bedsitter', _('Bedsitter')
+        ONE_BEDROOM = 'One_Bedroom',_('One_Bedroom')
+        TWO_BEDROOM = 'Two_Bedroom',_('Two_Bedroom')
+        THREE_BEDROOM = 'Three_Bedroom',_('Three_Bedroom')
+        APARTMENT = 'APARTMENT', _('Apartment')
+        HOUSE = 'HOUSE', _('House')
+        LAND = 'LAND', _('Land')
+        OFFICE = 'Office',_('Office')
+        STUDIO_APARTMENT = 'STUDIO_APARTMENT', _('Studio Apartment')
+        WAREHOUSE = 'Warehouse', _('Warehouse')
+        VILLA = 'VILLA', _('Villa')
+        CONDOMINIUM = 'CONDOMINIUM', _('Condominium')
+        COMMERCIAL = 'Commercial',_('Commercial')
+        FLAT = 'FLAT', _('Flat')
+        BUNGALOW = 'BUNGALOW', _('Bungalow')
+        PENTHOUSE = 'PENTHOUSE', _('Penthouse')
+        TERRACED_HOUSE = 'TERRACED_HOUSE', _('Terraced House')
+        TERRACED_FLAT = 'TERRACED_FLAT', _('Terraced Flat')
+        OTHER = 'Other', _('Other')
+
+    
+    user = models.ForeignKey(User, verbose_name= ("Agent,Renter,Seller or Buyer"),related_name= 'agent_buyer', on_delete=models.DO_NOTHING),
+    title = models.CharField(verbose_name=_('Property Title'), max_length=250)
+    slug = AutoSlugField(populate_from='title', unique=True,always_update= True)
+    ref_code = models.CharField(verbose_name=_("Property Reference Code"),max_length= 300, unique=True, blank= True,)
+    description = models.TextField(verbose_name=_("Description"), default="Default Description...Update description please...")
+    country = CountryField(verbose_name =_('Country'), default = "KE", blank_label = "(select Country)")
+
+    city = models.CharField(verbose_name=_('City'),max_length=200,default="Nairobi")
+    postal_code = models.CharField(verbose_name=_("Postal Code"),max_length=100, default= 150)
+    street_address = models.CharField(verbose_name=_('Street Address'),max_length=200,default="KMT Avenue")
+    property_number= models.IntegerField(verbose_name=_("Property Number"), validators=[MinValueValidator(0)],default=120)
+    price = models.DecimalField(verbose_name=_("Price"), max_digits=8, decimal_places=2,default=0.0)
+    tax = models.DecimalField(
+        verbose_name=_("Property Tax"),
+        max_digits=8,
+        decimal_places=2,
+        default=0.0,
+        help_text=_("Property Tax"),
+    )
+    plot_area= models.DecimalField(
+        verbose_name=_("Plot Area(m^2)"),
+        max_digits=8,
+        decimal_places=2,
+        default=0.0,
+        help_text=_("Plot Area"),
+    )
+
+    total_floors=models.IntegerField(
+        verbose_name=_("Total Floors"),
+        validators=[MinValueValidator(0)],
+        default=1,
+        help_text=_("Total Floors"),
+    )
+    bedroom= models.ImageField(
+        verbose_name=_("Bedroom"),
+        upload_to="property/bedroom",
+        default=1,
+        blank=True,
+        null=True,
+    )
+    bathroom= models.ImageField(
+        verbose_name=_("Bathroom"),
+        upload_to="property/bathroom",
+        blank=True,
+        max_length=4,
+        default = 1.0,
+    )
+
+    advert_type = models.CharField(
+        verbose_name=_("Advert Type"),
+        max_length=10,
+        choices=AdvertType.choices,
+        default=AdvertType.FOR_SALE,
+    )
+
+    property_type = models.CharField(
+        verbose_name=_("Property Type"),
+        max_length=40,
+        choices=PropertyType.choices,
+        default=PropertyType.SINGLE_ROOM,
+    )
+
+    cover_photo = models.ImageField(
+        verbose_name=_("Main Photo"),
+        upload_to="property/cover_photo",
+        blank=True,
+        null=True,
+        default="/cover_photo.jpg"
+    )
+
+    photo1 = models.ImageField(
+        verbose_name=_("Photo 1"),
+        upload_to="property/photo1",
+        default= "/interior_sample.jpg",
+        blank=True,
+    )
+
+    photo1 = models.ImageField(
+        verbose_name=_("Photo 2"),
+        upload_to="property/photo2",
+        default= "/interior_sample.jpg",
+        blank=True,
+    )
+
+    photo2 = models.ImageField(
+        verbose_name=_("Photo 3"),
+        upload_to="property/photo3",
+        default= "/interior_sample.jpg",
+        blank=True,
+    )
+    photo3 = models.ImageField(
+        verbose_name=_("Photo 3"),
+        upload_to="property/photo3",
+        default= "/interior_sample.jpg",
+        blank=True,
+    )
+    photo4 = models.ImageField(
+        verbose_name=_("Photo 3"),
+        upload_to="property/photo3",
+        default= "/interior_sample.jpg",
+        blank=True,
+    )
+
+    published_status = models.BooleanField(
+        verbose_name=_("Published Status"),
+        default=False,
+    )
+
+    views = models.IntegerField(
+        verbose_name=_("Views"),
+        default=0,
+    )
+
+    objects = models.Manager()
+    published = PropertyPublishedManager()
+
+    def __str__(self):
+        return self.title
+    
+    class Meta:
+        verbose_name = _("Property")
+        verbose_name_plural = _("Properties")
+
+    
+    def save(self, *args, **kwargs):
+        self.title = str.title(self.title)
+        self.description = str.title(self.description)
+        self.ref_code= " ".join(random.choices(
+            string.ascii_uppercase + string.digits, k=10
+        ))
+
+
+        super(Property, self).save(*args, **kwargs)
+
+    
+    @property
+    def final_property_price(self):
+        tax_percentage = self.tax
+        property_price = self.price
+        tax_amount = property_price * (tax_percentage / 100,2)
+        final_property_price = (property_price + tax_amount,2)
+        return final_property_price
+
+
+
+    
+
+
+
+
+
--- real_estate/settings/base.py
+++ real_estate/settings/base.py
@@ -41,7 +41,7 @@ THIRD_PARTY_APPS= [
     'django_countries',
     'phonenumber_field',
     'djoser',
-    'rest_framework_simplejwt'
+    'rest_framework_simplejwt',
 ]
 
 LOCAL_APPS = [
